884d7f3d14dec8abe70a57fa855be7d689c2427e
serviceandysu <42630273+serviceandysu@users.noreply.github.com>
2018-08-23 12:09:23 +0800

    Update test_webhook.js

--- test_webhook.js
+++ test_webhook.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({
 
 // init with auth
 line.init({
-  accessToken: '249P8WysFwyn7jWTU9Ibl8lVi465CaxqiIDAkOwScPP8hNUxKcXH5j+XN+O3jGzHyDGF9qRDtQWApyPKFcywzvADeU8lBfrOpakV91E7GuBisqZMJ96i6RrQOLENZ3Ef1GWKWOJ9by7uONRsWoJrNgdB04t89/1O/w1cDnyilFU=',
+  accessToken: '{249P8WysFwyn7jWTU9Ibl8lVi465CaxqiIDAkOwScPP8hNUxKcXH5j+XN+O3jGzHyDGF9qRDtQWApyPKFcywzvADeU8lBfrOpakV91E7GuBisqZMJ96i6RrQOLENZ3Ef1GWKWOJ9by7uONRsWoJrNgdB04t89/1O/w1cDnyilFU=}',
   // (Optional) for webhook signature validation
   channelSecret: '77cb22a8bb719dafd11d9739d3510584'
 })
